a6a0b74d7eaffd39a9c34e6374b578a7a99279c2
Abhishek Kadavergu <abhishekkadavergu@gmail.com>
2021-06-25 11:47:25 +0530

    CORS added in index.js file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const cors = require("cors");
+
 require("./db/mongoose");
 const userRoutes = require("../src/routers/user");
 const expenseRoutes = require("../src/routers/expense");
@@ -7,6 +9,8 @@ const incomeRoutes = require("../src/routers/income");
 const User = require("../src/models/user");
 
 const app = express();
+app.use(cors());
+
 app.use(express.json());
 app.use(userRoutes);
 app.use(expenseRoutes);
