efe2818ffecb442737c76415f18c568d2733d573
abhishekkadavergu <abhishekkadavergu@gmail.com>
2022-04-27 23:30:04 +0530

    password related routes updated

--- src/models/user.js
+++ src/models/user.js
@@ -5,59 +5,65 @@ const jwt = require("jsonwebtoken");
 const Expense = require("./expense");
 const Income = require("./income");
 
-const userSchema = mongoose.Schema(
-  {
+const userSchema = mongoose.Schema({
     name: {
-      type: String,
-      required: true,
+        type: String,
+        required: true,
     },
     email: {
-      type: String,
-      unique: true,
-      lowercase: true,
-      validate(value) {
-        if (!validator.isEmail(value)) {
-          throw new Error("Invalid email");
-        }
-      },
+        type: String,
+        unique: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error("Invalid email");
+            }
+        },
     },
     password: {
-      type: String,
-      required: true,
-      trim: true,
-      validate(value) {
-        if (value.includes("password")) {
-          throw new Error("Password should not contain the word password");
-        }
-        if (!validator.isLength(value, { min: 7 })) {
-          throw new Error("Password must be greater than 6 characters length");
-        }
-      },
+        type: String,
+        required: true,
+        trim: true,
+        validate(value) {
+            if (value.includes("password")) {
+                throw new Error("Password should not contain the word password");
+            }
+            if (!validator.isLength(value, { min: 7 })) {
+                throw new Error("Password must be greater than 6 characters length");
+            }
+        },
     },
     avatar: {
-      type: Buffer,
+        type: Buffer,
     },
-    tokens: [
-      {
+    tokens: [{
         token: {
-          type: String,
-          required: true,
+            type: String,
+            required: true,
         },
-      },
-    ],
+    }, ],
     age: {
-      type: Number,
-      validate(value) {
-        if (value < 0) {
-          throw new Error("Age shoild be positive");
-        }
-      },
+        type: Number,
+        validate(value) {
+            if (value < 0) {
+                throw new Error("Age should be positive");
+            }
+        },
     },
-  },
-  {
+    questions: [{
+        qst: {
+            type: String,
+            required: true
+        },
+        res: {
+            type: String,
+            required: true
+        }
+    }]
+
+}, {
     timestamps: true,
-  }
-);
+});
 
 // userSchema.virtual("tasks", {
 //   ref: "Tasks",
@@ -65,50 +71,51 @@ const userSchema = mongoose.Schema(
 //   foreignField: "owner",
 // });
 
-userSchema.methods.toJSON = function () {
-  const user = this;
-  const userObject = user.toObject();
+userSchema.methods.toJSON = function() {
+    const user = this;
+    const userObject = user.toObject();
 
-  delete userObject.password;
-  delete userObject.tokens;
-  delete userObject.avatar;
-  return userObject;
+    delete userObject.password;
+    delete userObject.tokens;
+    delete userObject.avatar;
+    delete userObject.questions
+    return userObject;
 };
 
-userSchema.methods.generateAuthToken = async function () {
-  const user = this;
-  const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
-  user.tokens = user.tokens.concat({ token });
-  await user.save();
-  return token;
+userSchema.methods.generateAuthToken = async function() {
+    const user = this;
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+    user.tokens = user.tokens.concat({ token });
+    await user.save();
+    return token;
 };
 
-userSchema.statics.findUserByCredentials = async (email, password) => {
-  const user = await User.findOne({ email });
-  if (!user) {
-    throw new Error("Unable to login");
-  }
-  const ismatch = await bcryptjs.compare(password, user.password);
-  if (!ismatch) {
-    throw new Error("Unable to login");
-  }
-  return user;
+userSchema.statics.findUserByCredentials = async(email, password) => {
+    const user = await User.findOne({ email });
+    if (!user) {
+        throw new Error("You are not registered");
+    }
+    const ismatch = await bcryptjs.compare(password, user.password);
+    if (!ismatch) {
+        throw new Error("Invalid credentials");
+    }
+    return user;
 };
 
-userSchema.pre("save", async function (next) {
-  const user = this;
-  if (user.isModified("password")) {
-    user.password = await bcryptjs.hash(user.password, 8);
-  }
-  console.log("Just before save!");
-  next();
+userSchema.pre("save", async function(next) {
+    const user = this;
+    if (user.isModified("password")) {
+        user.password = await bcryptjs.hash(user.password, 8);
+    }
+    console.log("Just before save!");
+    next();
 });
 
-userSchema.pre("remove", async function (next) {
-  const user = this;
-  await Expense.deleteMany({ owner: user._id });
-  await Income.deleteMany({ owner: user._id });
-  next();
+userSchema.pre("remove", async function(next) {
+    const user = this;
+    await Expense.deleteMany({ owner: user._id });
+    await Income.deleteMany({ owner: user._id });
+    next();
 });
 
 const User = mongoose.model("User", userSchema);
--- src/routers/user.js
+++ src/routers/user.js
@@ -6,54 +6,56 @@ const auth = require("../middleware/auth");
 
 const router = express.Router();
 
-router.post("/users", async (req, res) => {
-  const user = new User(req.body);
-  try {
-    await user.save();
-    const token = await user.generateAuthToken();
-    res.status(201).send({ user, token });
-  } catch (error) {
-    res.status(500).send(error);
-  }
+router.post("/users", async(req, res) => {
+    const user = new User(req.body);
+    try {
+        await user.save();
+        const token = await user.generateAuthToken();
+        res.status(201).send({ user, token });
+    } catch (error) {
+        res.status(500).send(error);
+    }
 });
 
-router.post("/users/login", async (req, res) => {
-  try {
-    const user = await User.findUserByCredentials(
-      req.body.email,
-      req.body.password
-    );
-    const token = await user.generateAuthToken();
-    res.send({ user, token });
-  } catch (error) {
-    res.status(400).send();
-  }
+const creds = multer()
+router.post("/users/login", creds.none(), async(req, res) => {
+    try {
+        const user = await User.findUserByCredentials(
+            req.body.email,
+            req.body.password
+        );
+        const token = await user.generateAuthToken();
+        res.send({ user, token });
+    } catch (error) {
+        res.status(400).send({ error: error.message });
+    }
 });
 
-router.post("/users/logout", auth, async (req, res) => {
-  try {
-    req.user.tokens = req.user.tokens.filter(
-      (token) => token.token !== req.token
-    );
-    await req.user.save();
-    res.send();
-  } catch (error) {
-    res.status(500).send();
-  }
+router.post("/users/logout", auth, async(req, res) => {
+    try {
+        console.log(req.user)
+        req.user.tokens = req.user.tokens.filter(
+            (token) => token.token !== req.token
+        );
+        await req.user.save();
+        res.send();
+    } catch (error) {
+        res.status(500).send();
+    }
 });
 
-router.post("/users/logoutAll", auth, async (req, res) => {
-  try {
-    req.user.tokens = [];
-    await req.user.save();
-    res.send();
-  } catch (error) {
-    res.status(500).send();
-  }
+router.post("/users/logoutAll", auth, async(req, res) => {
+    try {
+        req.user.tokens = [];
+        await req.user.save();
+        res.send();
+    } catch (error) {
+        res.status(500).send();
+    }
 });
 
-router.get("/users/me", auth, async (req, res) => {
-  res.send(req.user);
+router.get("/users/me", auth, async(req, res) => {
+    res.send(req.user);
 });
 
 // router.get("/users/:id", async (req, res) => {
@@ -70,88 +72,145 @@ router.get("/users/me", auth, async (req, res) => {
 //   }
 // });
 
-router.patch("/users/me", auth, async (req, res) => {
-  const updates = Object.keys(req.body);
-  const validUpdates = ["name", "email", "password", "age"];
-  const isValid = updates.every((update) => validUpdates.includes(update));
+router.patch("/users/me", auth, async(req, res) => {
+    const updates = Object.keys(req.body);
+    const validUpdates = ["name", "email", "password", "age"];
+    const isValid = updates.every((update) => validUpdates.includes(update));
 
-  if (!isValid) {
-    return res.status(404).send({ error: "invalid updates!" });
-  }
+    if (!isValid) {
+        return res.status(404).send({ error: "Invalid updates!" });
+    }
 
-  try {
-    // const user = await User.findById(req.params.id);
-    updates.forEach((update) => (req.user[update] = req.body[update]));
-    await req.user.save();
-    res.send(req.user);
-  } catch (error) {
-    res.status(500).send(error);
-  }
+    try {
+        // const user = await User.findById(req.params.id);
+        updates.forEach((update) => (req.user[update] = req.body[update]));
+        await req.user.save();
+        res.send(req.user);
+    } catch (error) {
+        res.status(500).send(error);
+    }
 });
 
-router.delete("/users/me", auth, async (req, res) => {
-  try {
-    req.user.remove();
-    res.send(req.user);
-  } catch (error) {
-    res.status(500).send(error);
-  }
+router.patch("/users/me/changepwd", auth, async(req, res) => {
+    const updates = Object.keys(req.body)
+    const validUpdates = ["curent_password", "new_password", "confirm_password"];
+    const isValid = updates.every(update => validUpdates.includes(update));
+    if (!isValid) {
+        return res.status(404).send({ error: "Bad inputs!" })
+    }
+
+    try {
+        const user = await User.findUserByCredentials(
+            req.user.email,
+            req.body.curent_password
+        );
+
+        user['password'] = req.body['confirm_password']
+        await user.save()
+        res.status(200).send({ 'success': "Password updated successfully" })
+    } catch (error) {
+        res.status(403).send({ error: "Invalid credentials" });
+    }
+})
+
+router.post("/users/forgotpwd", async(req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email })
+        if (!user) {
+            throw new Error("Couldn't find your email")
+        }
+        const zeroOrOne = Math.round(Math.random())
+        console.log(user['questions'][zeroOrOne])
+        res.status(200).send({ "question": { qst: user['questions'][zeroOrOne].qst, _id: user['questions'][zeroOrOne]._id }, email: user.email })
+
+    } catch (error) {
+        res.status(404).send({ error: error.message })
+
+    }
+
+})
+
+router.post("/users/setpwd", async(req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email, "questions._id": req.body.question._id, "questions.res": req.body.question.res })
+            // const user = await User.find({ "questions._id": req.body.question._id, "questions.res": req.body.question.res })
+        if (!user) {
+            throw new Error("Invalid response")
+        }
+
+        user['password'] = req.body.cpassword
+        await user.save()
+        res.status(200).send(user)
+
+    } catch (error) {
+        res.status(403).send(error.message)
+    }
+})
+
+router.delete("/users/me", auth, async(req, res) => {
+    try {
+        req.user.remove();
+        res.send(req.user);
+    } catch (error) {
+        res.status(500).send(error);
+    }
 });
 
 const upload = multer({
-  limits: {
-    fileSize: 1000000,
-  },
-  fileFilter(req, file, cb) {
-    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
-      return cb(new Error("Please upload jpg OR jpeg OR png files"));
-    }
-    cb(undefined, true);
-  },
+    limits: {
+        fileSize: 1000000,
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error("Please upload jpg OR jpeg OR png files"));
+        }
+        cb(undefined, true);
+    },
 });
 
 router.post(
-  "/users/me/avatar",
-  auth,
-  upload.single("avatar"),
-  async (req, res) => {
-    const buffer = await sharp(req.file.buffer)
-      .resize(250, 250)
-      .png()
-      .toBuffer();
-    req.user.avatar = buffer;
-    await req.user.save();
-    res.send();
-  },
-  (error, req, res, next) => {
-    res.status(400).send({ Error: error.message });
-  }
+    "/users/me/avatar",
+    auth,
+    upload.single("avatar"),
+    async(req, res) => {
+        const buffer = await sharp(req.file.buffer)
+            .resize(250, 250)
+            .png()
+            .toBuffer();
+        req.user.avatar = buffer;
+        await req.user.save();
+        res.send({ "success": "Photo uploaded successfully" });
+    },
+    (error, req, res, next) => {
+        res.status(400).send({ "fail": error.message });
+    }
 );
 
-router.delete("/users/me/avatar", auth, async (req, res) => {
-  try {
-    if (req.user.avatar) {
-      req.user.avatar = undefined;
-      await req.user.save();
-      res.send();
+router.delete("/users/me/avatar", auth, async(req, res) => {
+    try {
+        if (req.user.avatar) {
+            req.user.avatar = undefined;
+            await req.user.save();
+            res.status(200).send({ "success": "User avatar deleted successfully" });
+        }
+        res.status(404).send({ "fail": "No avatar found" });
+    } catch (error) {
+        res.status(500).send({ "fail": "Something went wrong" });
     }
-    res.status(404).send();
-  } catch (error) {
-    res.status(500).send();
-  }
 });
 
-router.get("/users/:id/avatar", async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id);
-    if (!user || !user.avatar) {
-      throw new Error();
+router.get("/users/:id/avatar", async(req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user || !user.avatar) {
+            throw new Error();
+        }
+        res.set("Content-Type", "image/jpg");
+        res.send(user.avatar);
+    } catch (error) {
+        res.status(500).send({ "fail": "Failed to get user photo" });
     }
-    res.set("Content-Type", "image/jpg");
-    res.send(user.avatar);
-  } catch (error) {
-    res.status(500).send();
-  }
 });
+
 
 module.exports = router;
